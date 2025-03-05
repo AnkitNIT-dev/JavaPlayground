7bde33d042e7d5ceb68d0a817a4684d84991b579
AnkitNIT-dev <tech.ankiteric@gmail.com>
2025-03-05 11:40:19 +0530

    feat: Add MainTest class to explain Java main method signature rules
    - Implemented a Java program to demonstrate why the main method signature cannot be changed
    - Added JavaDoc comments for better understanding
    - Included an example message in the main method output

---
+++ OOP/src/com/main/MainTest.java
@@ -0,0 +1,40 @@
+package com.main;
+
+/**
+ * This class demonstrates the importance of the standard main method signature
+ * in Java.
+ * 
+ * <p>
+ * In Java, the JVM (Java Virtual Machine) is responsible for executing
+ * programs. The JVM specifically looks for a predefined method signature to
+ * locate the entry point of execution.
+ * 
+ * <p>
+ * If we modify the standard main method signature, the JVM will not recognize
+ * it as the entry point and will throw an error indicating that the main method
+ * is missing.
+ * 
+ * <p>
+ * Standard main method signature:
+ * 
+ * <pre>
+ * public static void main(String[] args)
+ * </pre>
+ * 
+ * @author Your Name = Ankit Soni
+ */
+public class MainTest {
+
+	/**
+	 * The standard main method that serves as the entry point of the Java program.
+	 * 
+	 * <p>
+	 * This method prints a message regarding the effect of changing the main method
+	 * signature.
+	 *
+	 * @param args Command-line arguments (not used in this example)
+	 */
+	public static void main(String[] args) {
+		// your code is here
+	}
+}
